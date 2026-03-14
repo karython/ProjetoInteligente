d3e16de7d19b0e682fcf4575e48332e11c6905f1
viaconsmater <viaconsmater@gmail.com>
2026-03-13 22:45:37 -0300

    ajuste atualizacao de interface 5

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -4,9 +4,13 @@ const DashboardLayout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar />
+
       <main className="ml-64 p-8">
-        {children}
+        <div className="max-w-6xl mx-auto">
+          {children}
+        </div>
       </main>
+
     </div>
   )
 }
